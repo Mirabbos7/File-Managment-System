0b2a30c625fa473c1c9c9c05b6bcb760d5951e6e
Mirabbos7 <155135153+Mirabbos7@users.noreply.github.com>
2024-04-06 23:00:20 +0500

    Add files via upload

---
+++ main/java/com/file/managment/main/Breed.java
@@ -0,0 +1,29 @@
+package com.file.managment.main;
+
+public class Breed {
+    public enum CatBreed {
+        PERSIAN,
+        SIAMESE,
+        MAINE_COON,
+        RAGDOLL,
+        BENGAL,
+        SPHYNX,
+        BRITISH_SHORTHAIR,
+        SCOTTISH_FOLD,
+        ABYSSINIAN,
+        RAGAMUFFIN
+    }
+
+    public enum DogBreed{
+        LABRADOR,
+        GERMAN_SHEPHERD,
+        BULLDOG,
+        BEAGLE,
+        POODLE,
+        ROTTWEILER,
+        YORKSHIRE_TERRIER,
+        BOXER,
+        DACHSHUND,
+        SIBERIAN_HUSKY
+    }
+}
